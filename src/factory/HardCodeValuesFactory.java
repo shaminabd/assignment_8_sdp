fe3bf89f820ed345806d30b2b6d1abf77577e830
shaminabd <shynggys.abdullayev@gmail.com>
2025-11-09 19:27:31 +0500

    simplified the hardcoded values

--- src/factory/HardCodeValuesFactory.java
+++ src/factory/HardCodeValuesFactory.java
@@ -5,7 +5,6 @@ import cars.Minivan;
 import cars.Sedan;
 import cars.Truck;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HardCodeValuesFactory {
@@ -18,12 +17,10 @@ public class HardCodeValuesFactory {
     }
 
     public static List<Car> createVehicles() {
-        List<Car> vehicles = new ArrayList<>();
-
-        vehicles.add(new Minivan(20_000, 9, 75, 12, 180));
-        vehicles.add(new Truck(50_000, 7, 80, 15, 130));
-        vehicles.add(new Sedan(35_000, 5, 60, 8, 140));
-
-        return vehicles;
+        return List.of(
+            new Minivan(20_000, 9, 75, 12, 180),
+            new Truck(50_000, 7, 80, 15, 130),
+            new Sedan(35_000, 5, 60, 8, 140)
+        );
     }
 }
