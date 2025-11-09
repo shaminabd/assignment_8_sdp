1e361b1d82cecf209b7f98764c8852de3f23d002
shaminabd <shynggys.abdullayev@gmail.com>
2025-11-09 19:24:43 +0500

    changed the values for the correctnnes in the EstimationOfSellingPriceVisitor

--- src/visitors/EstimationOfSellingPriceVisitor.java
+++ src/visitors/EstimationOfSellingPriceVisitor.java
@@ -8,25 +8,25 @@ public class EstimationOfSellingPriceVisitor implements CarVisitor {
     //count formula is
     //base price - (wearCostPerKm * mileage of the car)
 
-    private double calculatePrice(double basePrice, double priceDropPerKm, double mileage) {
-        return basePrice - (priceDropPerKm * mileage);
+    private double calculatePrice(double basePrice, double wearCostPerKm, double mileage) {
+        return basePrice - (wearCostPerKm * mileage);
     }
 
     @Override
     public void visitTruck(Truck truck) {
-        double price = calculatePrice(25_000_000, 400, truck.getMileage());
-        System.out.println("Truck price in current condition is " + price + " ₸");
+        double price = calculatePrice(25_000_000, 40, truck.getMileage());
+        System.out.printf("Truck price in current condition is %.2f ₸%n", price);
     }
 
     @Override
     public void visitSedan(Sedan sedan) {
-        double price = calculatePrice(9_000_000, 100, sedan.getMileage());
-        System.out.println("Sedan price in current condition is " + price + " ₸");
+        double price = calculatePrice(9_000_000, 30, sedan.getMileage());
+        System.out.printf("Sedan price in current condition is %.2f ₸%n", price);
     }
 
     @Override
     public void visitMinivan(Minivan minivan) {
-        double price = calculatePrice(13_000_000, 150, minivan.getMileage());
-        System.out.println("Minivan price in current condition is " + price + " ₸");
+        double price = calculatePrice(13_000_000, 25, minivan.getMileage());
+        System.out.printf("Minivan price in current condition is %.2f ₸%n", price);
     }
 }
